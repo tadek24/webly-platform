2e3f253aefbcdc781c231150a12435c2819a8d0d
tadek24 <tadekkw123@gmail.com>
2026-08-14 12:29:44 +0200

    Add marketing content workspace

---
+++ src/app/panel/tresci/page.tsx
@@ -0,0 +1,4 @@
+import { ContentWorkspace } from "@/components/content-workspace";
+
+export default function ContentPage() { return <main className="studio-main content-main"><ContentWorkspace /></main>; }
+
