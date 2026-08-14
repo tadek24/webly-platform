a111cc069fc591962cc5e6fa18d28853ecb2f20c
tadek24 <tadekkw123@gmail.com>
2026-08-14 15:34:30 +0200

    Connect customer plans to pricing data

--- src/app/konto/subskrypcja/page.tsx
+++ src/app/konto/subskrypcja/page.tsx
@@ -1,10 +1,10 @@
 import { SubscriptionManager } from "@/components/subscription-manager";
-import { getSubscription } from "@/lib/customer-data";
+import { getPackages, getSubscription } from "@/lib/customer-data";
 import { requireCustomerSession } from "@/lib/customer-session";
+import { DEFAULT_PACKAGES } from "@/lib/package-defaults";
 
 export default async function SubscriptionPage() {
   const customer = await requireCustomerSession();
-  const subscription = await getSubscription(customer.id);
-  return <main className="account-main"><section className="account-page-title"><span>ABONAMENT</span><h1>Prosto, miesięcznie,<br /><em>bez technicznego chaosu.</em></h1><p>Zmiana planu zostanie zapisana od razu. Automatyczne płatności podłączymy do WooCommerce w następnym etapie.</p></section><SubscriptionManager initial={subscription} /></main>;
+  const [subscription, packages] = await Promise.all([getSubscription(customer.id), getPackages().catch(() => DEFAULT_PACKAGES)]);
+  return <main className="account-main"><section className="account-page-title"><span>ABONAMENT</span><h1>Prosto, miesięcznie,<br /><em>bez technicznego chaosu.</em></h1><p>Wybierasz zakres usługi, a Webly zajmuje się hostingiem, aktualizacjami i zapleczem. Niczego nie instalujesz.</p></section><SubscriptionManager initial={subscription} packages={packages} /></main>;
 }
-
